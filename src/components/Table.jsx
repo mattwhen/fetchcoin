8029e3c3289f0a47c2d6871c307316367d33132a
Matthew Nguyen <76631936+mattwhen@users.noreply.github.com>
2023-08-10 22:59:18 -0500

    This commit adds another useState hook for loading state that displays to the user content is loading, and displaying our JSON data into the table.

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -2,33 +2,31 @@ import React from 'react';
 import { useEffect, useState } from 'react';
 
 const Table = () => {
+	const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
 
-    const [data, setData] = useState([]);
+	useEffect(() => {
+		// Called above the returned JSX syntax
+		// Pass two arguments into our useEffect() hook: a Function and Array.
+		// callback function is called after the component renders.
+		// The second argument is called our dependencies array. This array should include all of the values that our side effect relies upon.
+		const url = 'https://api.coinstats.app/public/v1/coins?skip=0&limit=10';
+		fetch(url)
+			.then((response) => response.json())
+			.then((json) => {
+				setData(json);
+                setLoading(false);
+			})
+			.catch((error) => console.error(error));
+	}, []);
 
-      useEffect(() => {
-        // Called above the returned JSX syntax
-        // Pass two arguments into our useEffect() hook: a Function and Array.
-        // callback function is called after the component renders.
-        // The second argument is called our dependencies array. This array should include all of the values that our side effect relies upon. 
+	if (loading) return <h1>Fetching data...</h1>; // While fetching data, display message to user that the data is currently trying to render.
 
-        const url = 'https://api.coinstats.app/public/v1/coins?skip=0&limit=10'
-        
-        function getData() {
-
-            fetch(url)
-            .then(response => response.json())
-            .then(json => setData(json))
-            .then(data => data.coins.map(item => console.log(item)));
-        };
-        
-        getData();
-
-      }, []);
-
+	console.log('data initial state', data.coins);
 
 	return (
 		<div className='overflow-x-auto border-2 border-slate-950 rounded-sm mx-2'>
-			<table className=' table-auto'>
+			<table className=''>
 				<thead>
 					<tr>
 						<th className=' bg-slate-500 text-white'>Rank</th>
@@ -36,16 +34,24 @@ const Table = () => {
 						<th className=' bg-slate-500 text-white'>Price</th>
 					</tr>
 				</thead>
-                <tbody>
-                    <tr>
-                        {/* <td>{data.coins}</td> */}
-                        <td>data data data datat datadtasdt</td>
-                        <td>data data data datat datadtasdt</td>
-                        <td>data data data datat datadtasdt</td>
-                        <td>data data data datat datadtasdt</td>
-                        <td>data data data datat datadtasdt</td>
-                    </tr>
-                </tbody>
+				<tbody>
+                    {data.coins.map(item => (
+                        <>
+                        <tr>
+                        <td>
+                            <div className='flex justify-between'>
+                            {item.rank}
+                        <img src={item.icon} className='w-8' alt='crypto-icon'></img>
+                        {item.name}
+
+                            </div>
+                        
+                        </td>
+                        </tr>
+                        </>
+                    ))}
+
+				</tbody>
 			</table>
 		</div>
 	);

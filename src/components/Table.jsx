686fd79581827db90ab8b4abe9808a47b450095e
Matthew Nguyen <76631936+mattwhen@users.noreply.github.com>
2023-08-12 21:04:44 -0500

    This commit adds Crypto ticker symbol

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -20,9 +20,9 @@ const Table = () => {
 			.catch((error) => console.error(error));
 	}, []);
 
-    function numberWithCommas(num) {
-        return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-      }
+	function numberWithCommas(num) {
+		return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+	}
 
 	if (loading) return <h1>Fetching data...</h1>; // While fetching data, display message to user that the data is currently trying to render.
 
@@ -43,26 +43,29 @@ const Table = () => {
 					{data.coins.map((item) => (
 						<>
 							<tr>
-                                <td className='RANK'>
-                                    <div className='mx-5'>{item.rank}</div>
-                                </td>
+								<td className='RANK'>
+									<div className='p-5'>{item.rank}</div>
+								</td>
 								<td>
-									<div className='ICON'>
-										<img
-											src={item.icon}
-											className='w-8'
-											alt='crypto-icon'
-										></img>
-										{item.name}
+									<div className='NAME flex items-center pl-5 mr-10 w-32'>
+										<div className='text-left'>
+											<img
+												src={item.icon}
+												className='w-8 h-8 mr-4'
+												alt='crypto-icon'
+											></img>
+										</div>
+                                        <div className='flex flex-col'>
+										<span className='text-md'>{item.symbol}</span>
+										<span className='text-sm'>{item.name}</span>
+                                        </div>
 									</div>
 								</td>
-                                <td className='PRICE'>
-                                    {/* toFixed() method rounds the number to the 2 decimal places */}
-                                    ${item.price.toFixed(2)} 
-                                </td>
-                                <td>
-                                    ${numberWithCommas(item.marketCap)}
-                                </td>
+								<td className='PRICE'>
+									{/* toFixed() method rounds the number to the 2 decimal places */}
+									${item.price.toFixed(2)}
+								</td>
+								<td>${numberWithCommas(item.marketCap)}</td>
 							</tr>
 						</>
 					))}

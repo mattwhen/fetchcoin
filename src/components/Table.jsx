a7e6d439e32441773ba93aeae71c2e83c4f4c95d
Matthew Nguyen <76631936+mattwhen@users.noreply.github.com>
2023-08-14 21:27:36 -0500

    This commit adds a few comments, and adds class using ternary operator to the Price Change column

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -20,26 +20,42 @@ const Table = () => {
 			.catch((error) => console.error(error));
 	}, []);
 
+	// Adds comma formatting to current Price and Market Cap columns.
 	function numberWithCommas(num) {
 		return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 	}
 
-	if (loading) return <h1>Fetching data...</h1>; // While fetching data, display message to user that the data is currently trying to render.
+	// While fetching data, display message to user that the data is currently trying to render itself.
+	if (loading) return <h1>Fetching data...</h1>;
 
-	console.log('data initial state', data.coins);
+	// <------------------------------------ REMOVE WHEN COMPLETE ------------------------------>
+	console.log('COIN DATA', data.coins);
 
 	return (
+		// Table Headers section
 		<div className='overflow-x-auto border-2 border-slate-300 rounded-md mx-2'>
 			<table className='table-auto'>
 				<thead>
 					<tr>
-						<th className=' text-gray-500 font-medium text-left pl-2 pt-3'>Rank</th>
-						<th className=' text-gray-500 font-medium text-left pl-2 pt-3'>Name</th>
-						<th className=' text-gray-500 font-medium text-left pr-8 pt-3'>Price</th>
-						<th className=' text-gray-500 font-medium text-left pt-3'>Market Cap</th>
+						<th className=' text-gray-500 font-medium text-left pl-2 pt-3'>
+							Rank
+						</th>
+						<th className=' text-gray-500 font-medium text-left pl-2 pt-3'>
+							Name
+						</th>
+						<th className=' text-gray-500 font-medium text-left pr-8 pt-3'>
+							Price
+						</th>
+						<th className=' text-gray-500 font-medium text-left pr-8 pt-3'>
+							Chg %
+						</th>
+						<th className=' text-gray-500 font-medium text-left pt-3'>
+							Market Cap
+						</th>
 					</tr>
 				</thead>
 				<tbody>
+					{/* Render Coinstats API data */}
 					{data.coins.map((item) => (
 						<>
 							<tr>
@@ -56,25 +72,37 @@ const Table = () => {
 											></img>
 										</div>
 										<div className='flex flex-col'>
-											<span className='text-sm font-bold leading-4 text-left'>{item.name}</span>
+											<span className='text-sm font-bold leading-4 text-left'>
+												{item.name}
+											</span>
 											<span className='text-sm font-semi'>{item.symbol}</span>
 										</div>
 									</div>
 								</td>
 								<td className='PRICE'>
-									<div className="w-40">
-									<span className='text-right font-semi'>
-										{/* Rounds price to two decimal places */}$
-										{numberWithCommas(item.price.toFixed(2))}
-									</span>
+									<div className='w-40'>
+										<span className='text-right font-semi'>
+											{/* Rounds price to two decimal places */}$
+											{numberWithCommas(item.price.toFixed(2))}
+										</span>
 									</div>
 								</td>
 								<td>
-									<span className='font-semi'>
-									${numberWithCommas(item.marketCap.toFixed(2))}
-
+									<span
+									// Sets the class depending on whether the price is POSITIVE or NEGATIVE. 
+										className={
+											item.priceChange1d > 0 ? 'green-change' : 'red-change'
+										}
+									>
+										{item.priceChange1d}
 									</span>
-									</td>
+								</td>
+								<td>
+									<span className='font-semi'>
+										{/* Rounds price to two decimal places */}$
+										{numberWithCommas(item.marketCap.toFixed(2))}
+									</span>
+								</td>
 							</tr>
 						</>
 					))}

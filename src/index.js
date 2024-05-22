e8f5d3a41ce98356c0f6ec4a65e0c57d2701f1a2
Matthew Nguyen <76631936+mattwhen@users.noreply.github.com>
2024-05-21 22:20:43 -0500

    Initialize Firebase

--- src/index.js
+++ src/index.js
@@ -5,8 +5,26 @@ import App from './App';
 import ErrorPage from './components/ErrorPage/ErrorPage';
 import './App.css';
 import CoinRoute from './Routes/CoinRoute/CoinRoute';
-import Login from './components/Login/Login';
-import Signup from './components/Signup/Signup';
+import Login from './Routes/Login/Login';
+import Signup from './Routes/Signup/Signup';
+
+// Initialize Firebase and create a Firebase App object
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+	apiKey: "AIzaSyBADrE_egtaqi6UndL4gtXYpC4vIgO19jQ",
+	authDomain: "fetchcoin-56235.firebaseapp.com",
+	projectId: "fetchcoin-56235",
+	storageBucket: "fetchcoin-56235.appspot.com",
+	messagingSenderId: "329155771608",
+	appId: "1:329155771608:web:2aff17272a64a64d736bad"
+  };
+  
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
+  const auth = getAuth(app);
 
 const router = createBrowserRouter([
 	{
@@ -28,7 +46,7 @@ const router = createBrowserRouter([
 		path: '/signup',
 		element: <Signup />,
 		errorElement: <ErrorPage />,
-	}
+	},
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(

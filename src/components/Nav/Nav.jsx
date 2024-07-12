552878c274532a04cee18748764830b73747f184
Matthew Nguyen <76631936+mattwhen@users.noreply.github.com>
2024-07-11 21:31:01 -0500

    This commit will implement the sign-in and sign-up functionality using Firebase.

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,77 +1,91 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import LightLogo from '../../images/Logo_whiteBg.png';
-import { useState, useEffect } from 'react';
-import { GiHamburgerMenu } from 'react-icons/gi';
-import { RxCross1 } from 'react-icons/rx';
-import '../../App.css';
-import './Nav.css';
+import { getAuth, signOut } from "firebase/auth";
+import { Link } from "react-router-dom";
+import LightLogo from "../../images/Logo_whiteBg.png";
+import { useState, useEffect } from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { RxCross1 } from "react-icons/rx";
+import "../../App.css";
+import "./Nav.css";
 
 const Nav = () => {
 	const [mobileOpen, setMobileOpen] = useState(false);
 	const [showNav, setShowNav] = useState(true);
-	// const [previousScroll, setPreviousScroll] = useState(0);
 
 	// Prevent/hide scrolling when the Navigation menu is opened.
 	const mobileOpenHandler = () => {
 		if (mobileOpen) {
-			document.getElementsByTagName('body')[0].style.overflow = 'visible';
+			document.getElementsByTagName("body")[0].style.overflow = "visible";
 			setMobileOpen(false);
 		} else {
-			document.getElementsByTagName('body')[0].style.overflow = 'hidden';
+			document.getElementsByTagName("body")[0].style.overflow = "hidden";
 			setMobileOpen(true);
 		}
+	};
+
+	const auth = getAuth();
+
+	const handleSignOut = () => {
+		signOut(auth)
+			.then(() => {
+				// Sign-out successful.
+				console.log("Sign-out successful", auth);
+				window.location.href = "/login";
+			})
+			.catch((error) => {
+				// An error happened.
+				console.error("Error signing out", error);
+			});
 	};
 
 	const navLinks = [
 		{
 			id: 0,
-			title: 'ABOUT',
-			link: '/fetchcoin/#about',
+			title: "ABOUT",
+			link: "/fetchcoin/#about",
 		},
 		{
 			id: 1,
-			title: 'CRYPTO',
-			link: '/fetchcoin/#crypto',
+			title: "CRYPTO",
+			link: "/fetchcoin/#crypto",
 		},
 		{
 			id: 2,
-			title: 'MISSION',
-			link: '/fetchcoin/#mission',
+			title: "MISSION",
+			link: "/fetchcoin/#mission",
 		},
 		{
 			id: 3,
-			title: 'CONTACT',
-			link: '/fetchcoin/#contact',
+			title: "CONTACT",
+			link: "/fetchcoin/#contact",
 		},
 	];
 
 	return (
 		<>
-			<nav className='bg-white sticky top-0 z-50 h-16 max-w-[1920px] shadow-sm'>
+			<nav className="bg-white sticky top-0 z-50 h-16 max-w-[1920px] shadow-sm">
 				{/* DESKTOP NAVIGATION LINKS */}
 				<div
-					className='hidden w-full px-4 text-black lg:flex lg:items-center lg:justify-center'
-					id='desktopUl'
+					className="hidden w-full px-4 text-black lg:flex lg:items-center lg:justify-center"
+					id="desktopUl"
 				>
-					<div className='logo lg:flex lg:justify-center'>
-						<a href='/fetchcoin' className=''>
+					<div className="logo lg:flex lg:justify-center">
+						<a href="/fetchcoin" className="">
 							<img
 								src={LightLogo}
-								className='cursor-pointer'
-								alt='Company Logo'
+								className="cursor-pointer"
+								alt="Company Logo"
 								width={150}
 								height={150}
 							></img>
 						</a>
 					</div>
-					<div className='nav-links lg:flex lg:justify-center'>
+					<div className="nav-links lg:flex lg:justify-center">
 						{navLinks.map(({ id, title, link }) => {
 							return (
 								<a
 									key={id}
 									href={link}
-									className='mx-5 hvr-underline-from-left hvr-underline-from-left:before hvr-underline-from-left:focus:before hvr-underline-from-left:focus:active'
+									className="mx-5 hvr-underline-from-left hvr-underline-from-left:before hvr-underline-from-left:focus:before hvr-underline-from-left:focus:active"
 								>
 									{title}
 								</a>
@@ -79,51 +93,62 @@ const Nav = () => {
 						})}
 					</div>
 					{/* Log in & Sign up buttons */}
-					<div className='hidden lg:flex'>
-						<Link to='/login'>
+					<div className="hidden lg:flex">
+						{!auth.currentUser ? (
+							<Link to="/login">
+								<button className="log-in rounded-full mr-2 text-center w-24 bg-blue-background text-white py-2 px-4 hover:bg-blue-background-hover">
+									Log in
+								</button>
+							</Link>
+						) : null}
+						{!auth.currentUser ? (
+							<Link to="/signup">
+								<button className="sign-up rounded-full text-center w-24 bg-gold-trim text-white py-2 px-4 hover:bg-gold-trim-hover">
+									Sign up
+								</button>
+							</Link>
+						) : null}
+						{auth.currentUser ? (
 							<button
-								className='log-in rounded-full mr-2 text-center w-24 bg-blue-background text-white py-2 px-4 hover:bg-blue-background-hover'
+								onClick={handleSignOut}
+								className="rounded-full text-center w-24 bg-gold-trim text-white py-2 px-4 hover:bg-gold-trim-hover"
 							>
-								Log in
+								Sign Out
 							</button>
-						</Link>
-						<Link to='/signup'>
-							<button
-								className='sign-up rounded-full text-center w-24 bg-gold-trim text-white py-2 px-4 hover:bg-gold-trim-hover'
-							>
-								Sign up
-							</button>
-						</Link>
+						) : null}
+						<div>
+						{/* {auth.currentUser.email ? <span>{auth.currentUser.email}</span> : null} */}
+						</div>
 					</div>
 				</div>
 				{/* Mobile view */}
-				<div className='flex items-center px-4 lg:hidden'>
+				<div className="flex items-center px-4 lg:hidden">
 					<img
 						src={LightLogo}
-						className='cursor-pointer'
-						alt='Company Logo'
+						className="cursor-pointer"
+						alt="Company Logo"
 						width={150}
 						height={150}
 						onClick={() =>
-							(window.location.href = 'https://mattwhen.github.io/fetchcoin/')
+							(window.location.href = "https://mattwhen.github.io/fetchcoin/")
 						}
 					></img>
 					<div
-						id='hamburger-menu'
-						className='text-4xl flex align-middle text-gray-500 ml-auto lg:hidden'
+						id="hamburger-menu"
+						className="text-4xl flex align-middle text-gray-500 ml-auto lg:hidden"
 						onClick={mobileOpenHandler}
 					>
 						{mobileOpen ? (
-							<RxCross1 className='z-50 transition-all text-gray-700' />
+							<RxCross1 className="z-50 transition-all text-gray-700" />
 						) : (
-							<GiHamburgerMenu className=' text-gray-700' />
+							<GiHamburgerMenu className=" text-gray-700" />
 						)}
 					</div>
 				</div>
 				{/* HAMBURGER MENU OPTIONS */}
 				<ul
 					className={
-						mobileOpen ? 'show-mobile-nav bg-white ' : 'hide-mobile-nav'
+						mobileOpen ? "show-mobile-nav bg-white " : "hide-mobile-nav"
 					}
 				>
 					{navLinks.map(({ id, title, link }) => {

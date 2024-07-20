24c18a68433334f505d2b95ad07aa8f909d97029
Matthew Nguyen <76631936+mattwhen@users.noreply.github.com>
2024-07-20 16:54:05 -0500

    This commit fixes pagination bug where it would display a negative page number or page number greater than the length of the data.

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -6,40 +6,36 @@ import "./Pagination.css";
 const Pagination = ({ data, page, setPage, numOfCoinsPerPage }) => {
 	const [currentRange, setCurrentRange] = useState([1, 2, 3]);
 
+	const firstPage = 1;
+	const lastPage = data.length / numOfCoinsPerPage;
+
+
 	const handleNextClick = () => {
-		if (page === 50) {
+		if (page === lastPage) {
 			return null;
 		}
 
 		const newPage = page + 1;
 		const isInRange = currentRange.includes(newPage);
-		console.log(newPage);
 
 		if (isInRange) {
 			setPage(page + 1);
-		}
-
-		// IF newPage is NOT within the range, we update the currentRange.
-		else {
-			// Update the currentRange with a new set of numbers that extend from the previous set of numbers (e.g. 1-5 to 6-10).
-			// Use map() to increment the currentRange by one.
-			// Update setPage() to the new data range.
+		} else {
 			setPage(page + 1);
 			setCurrentRange(currentRange.map((num) => num + 1));
 		}
 	};
 
-	const handlePreviousClick = () => {
+	const handlePreviousClick = () => { 
 		// If the previous page falls outside of the currentRange, do something.
 		// Update the current range values to the last five digits.
 
 		const prevPage = page - 1; // [6, 7, 8, 9, 10] => [1, 2, 3, 4, 5]
 		const isInRange = currentRange.includes(prevPage);
 
-		if (page === 1) {
+		if (page === firstPage) {
 			return null;
 		}
-
 		if (isInRange) {
 			setPage(page - 1);
 		} else {
@@ -50,30 +46,46 @@ const Pagination = ({ data, page, setPage, numOfCoinsPerPage }) => {
 
 	const displayFirstPage = () => {
 		setPage(1);
-		setCurrentRange([1, 2, 3, 4, 5]);
+		setCurrentRange([1, 2, 3]);
 	};
 
 	const handlePageSkipAhead = () => {
-		// IF page is greater than 50? Don't do anything
-		if (page > 50) {
+		if (page > lastPage) {
 			return null;
 		}
-		if(page + 5 < data.length / numOfCoinsPerPage) {
+		
+		if (page + 5 >= lastPage) {
+			setPage(lastPage);
+			setCurrentRange([lastPage - 2, lastPage - 1, lastPage]);
+		}
+		if (page + 5 <= lastPage) {
 			setPage(page + 5);
-			setCurrentRange(currentRange.map((num) => num + 5))
+			setCurrentRange(currentRange.map((page) => page + 5));
 		}
 	};
 
+	// TODO: implement logic similar to handlePageSkipAhead but reverse.
 	const handlePageSkipBehind = () => {
-		// If page is less than 1? Don't do anything.
-		if (page < 1) {
+		if (page < firstPage) {
 			return null;
+		}
+		// Check IF the current page - 5 is less than 1. 
+		// If it is, automatically set the page to 1. If we don't do this, the page jumps to a negative number. 
+		// Otherwise, set the page - 5 and update the page range.
+		if (page - 5 <= firstPage) {
+			setPage(firstPage);
+			setCurrentRange([firstPage, firstPage + 1, firstPage + 2])
+		} else {
+			setPage(page - 5);
+			setCurrentRange(currentRange.map((page) => page - 5))
 		}
 	};
 
 	const handleDisplayLastPage = () => {
-		setPage(data.length / numOfCoinsPerPage);
+		setPage(lastPage);
+		setCurrentRange([lastPage - 2, lastPage - 1, lastPage]);
 	};
+
 	return (
 		<>
 			<div className="flex justify-center my-5 lg:max-w-4xl lg:m-auto lg:mt-5 lg:flex">
@@ -85,7 +97,6 @@ const Pagination = ({ data, page, setPage, numOfCoinsPerPage }) => {
 					>
 						<FaArrowLeftLong className="w-6 h-6" />
 					</li>
-					{/* Not visible until page count is greater than 5 */}
 					{page > 3 ? (
 						<li
 							className="pagination-item pagination-item:hover"
@@ -105,8 +116,9 @@ const Pagination = ({ data, page, setPage, numOfCoinsPerPage }) => {
 						</li>
 					) : null}
 					{/* Not visible until page count is greater than 5 */}
-					{page > 5
-						? currentRange.slice(2, 5).map((num, index) => (
+					{/* TODO: set the current page to be in between the number before and after. e.g. 5, (6), 7 or 8, (9), 10 */}
+					{page > 3
+						? currentRange.map((num, index) => (
 								<li
 									// Apply styling based on the current page being selected.
 									onClick={() => setPage(num)}
@@ -120,18 +132,18 @@ const Pagination = ({ data, page, setPage, numOfCoinsPerPage }) => {
 								</li>
 						  ))
 						: currentRange.map((num, index) => (
-							<li
-								// Apply styling based on the current page being selected.
-								onClick={() => setPage(num)}
-								className={
-									page === num
-										? "selected-page"
-										: "pagination-item pagination-item:hover"
-								}
-							>
-								{num}
-							</li>
-					  ))}
+								<li
+									// Apply styling based on the current page being selected.
+									onClick={() => setPage(num)}
+									className={
+										page === num
+											? "selected-page"
+											: "pagination-item pagination-item:hover"
+									}
+								>
+									{num}
+								</li>
+						  ))}
 					<li
 						className="pagination-item pagination-item:hover"
 						onClick={handlePageSkipAhead}
@@ -144,7 +156,7 @@ const Pagination = ({ data, page, setPage, numOfCoinsPerPage }) => {
 						onClick={handleDisplayLastPage}
 					>
 						{/* Displays the LAST page of Coin data */}
-						{data.length / numOfCoinsPerPage}
+						{lastPage}
 					</li>
 					{/* Renders the NEXT page of Coin data */}
 					<li className="hidden md:cursor-pointer md:ml-8 md:flex md:align-middle">
